fdcdcffebdbef6ac79241d99f5a124e0290f2cfc
zaydaadam <kuzanzaokiji@outlook.com>
2026-04-13 19:29:26 -0400

    Fixed user not showing Topbar

--- components/ui/Topbar.jsx
+++ components/ui/Topbar.jsx
@@ -1,17 +1,30 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import { logout } from "@/lib/auth/logout";
 
 export default function Topbar() {
   const router = useRouter();
+
+  // store logged in user
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const savedUser = localStorage.getItem("user");
+
+    if (savedUser) {
+      const parsedUser = JSON.parse(savedUser);
+      setUser(parsedUser);
+    }
+  }, []);
 
   return (
     <header className="topbar">
       <h2>Welcome</h2>
 
       <div className="topbar-right">
-        <span>User</span>
+        <span>{user ? user.username : "User"}</span>
         <button type="button" onClick={() => logout(router)}>
           Logout
         </button>
